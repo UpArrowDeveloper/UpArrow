c3160d45913e6b1cf28b91850d255e00d823d763
pkiop <pkiopb@gmail.com>
2023-03-29 22:00:43 +0900

    rebuild

--- UpArrow-Frontend/Layouts/Main.js
+++ UpArrow-Frontend/Layouts/Main.js
@@ -15,8 +15,7 @@ const MainLayoutBlock = styled.main`
 const MainLayout = ({ children }) => {
   const stockRef = useRef(null);
   const ideaRef = useRef(null);
-  const investorRef = useRef(null);
-
+e const investorRef = useRef(null);
   return (
     <MainLayoutBlock className='mainlayout'>
       <Navbar
