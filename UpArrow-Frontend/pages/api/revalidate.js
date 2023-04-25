95724b3166066610f5dc26d8a6fd93bb607ee8b6
pkiop <pkiopb@gmail.com>
2023-04-25 23:01:47 +0900

    delete log

--- UpArrow-Frontend/pages/api/revalidate.js
+++ UpArrow-Frontend/pages/api/revalidate.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  console.log('res : ', res);
   await res.unstable_revalidate('/');
   await res.unstable_revalidate('/stock');
   await res.unstable_revalidate('/idea');
