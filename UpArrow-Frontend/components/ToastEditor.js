c886ffea8b40c8ebaba7e394ada8a11001d465f1
pkiop <pkiopb@gmail.com>
2023-06-21 23:31:23 +0900

    add custom button on editor

--- UpArrow-Frontend/components/ToastEditor.js
+++ UpArrow-Frontend/components/ToastEditor.js
@@ -1,19 +1,112 @@
 import styled from "@emotion/styled";
 import { Editor } from "@toast-ui/react-editor";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { mobileWidth } from "../styles/responsive";
 import { useMobile } from "../hooks/useMobile";
+import VideoUploader from "./VideoUploader";
+import color from "../styles/color";
 
 const ToastEditorBlock = styled.div`
   .toastui-editor-contents {
     font-size: 1.2rem;
     font-family: "Pretendard";
   }
+
+  .video-input {
+    width: 80%;
+    position: fixed;
+    z-index: 40;
+    border: 0.1rem solid ${color.B80};
+    padding: 0.72rem 1.2rem 0.72rem 2rem;
+    top: 50%;
+    border-radius: 999rem;
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      color: ${color.B80};
+    }
+  }
+
+  .video-icon-wrapper {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 30;
+  }
+
+  @media screen and (max-width: ${mobileWidth}) {
+    .video-input {
+      position: fixed;
+      width: calc(100% - 2rem);
+
+      left: 1rem;
+    }
+  }
 `;
 
-const ToastEditor = ({ content, setPostForm, placeholder }) => {
+const ToastEditor = ({
+  content,
+  setPostForm,
+  placeholder,
+  setFile,
+  url,
+  setUrl,
+}) => {
   const editorRef = useRef();
+  const inputImageRef = useRef();
   const { isMobile } = useMobile();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    editorRef.current?.getInstance()?.insertToolbarItem(
+      { groupIndex: 0, itemIndex: 0 },
+      {
+        name: "myItem",
+        tooltip: "video",
+        command: "bold",
+        text: "",
+        className: "toastui-editor-toolbar-icons video",
+        style: {
+          backgroundImage: `url(/images/Video.svg)`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        },
+      }
+    );
+    editorRef.current?.getInstance()?.insertToolbarItem(
+      { groupIndex: 0, itemIndex: 0 },
+      {
+        name: "myItem",
+        tooltip: "picture",
+        command: "bold",
+        text: "",
+        className: "toastui-editor-toolbar-icons picture",
+        style: {
+          backgroundImage: `url(/images/Picture.svg)`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        },
+      }
+    );
+
+    editorRef.current
+      .getRootElement()
+      .querySelector(".video")
+      .addEventListener("click", (e) => {
+        setIsOpen(true);
+      });
+
+    editorRef.current
+      .getRootElement()
+      .querySelector(".picture")
+      .addEventListener("click", (e) => {
+        inputImageRef.current.click();
+      });
+  }, []);
+
   return (
     <ToastEditorBlock>
       <Editor
@@ -34,6 +127,35 @@ const ToastEditor = ({ content, setPostForm, placeholder }) => {
         useCommandShortcut={true}
         ref={editorRef}
       />
+      <input
+        ref={inputImageRef}
+        className="image-input"
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        onChange={(e) => {
+          const f = e.target.files?.[0];
+          if (!f) return;
+          const url = URL.createObjectURL(f);
+          setFile({ file: f, url });
+        }}
+      ></input>
+      {isOpen && (
+        <input
+          className="video-input"
+          value={url}
+          placeholder={placeholder}
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        />
+      )}
+      {isOpen && (
+        <div
+          className="video-icon-wrapper"
+          onClick={() => setIsOpen(false)}
+        ></div>
+      )}
     </ToastEditorBlock>
   );
 };
