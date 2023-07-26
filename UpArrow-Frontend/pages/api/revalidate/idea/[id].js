9e5199a29724902a7dc6fbfb2c2e1b80ff318be8
pkiop <pkiopb@gmail.com>
2023-07-26 22:20:33 +0900

    d

--- UpArrow-Frontend/pages/api/revalidate/idea/[id].js
+++ UpArrow-Frontend/pages/api/revalidate/idea/[id].js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
   console.log("req.params : ", req.params);
   console.log("req.query : ", req.query);
   console.log("req.body : ", req.body);
-  if (!req.params?.id) {
+  if (!req.query?.id) {
     return res.status(400).json({ message: "missing id" });
   }
-  await res.unstable_revalidate("/idea/" + req.params.id);
+  await res.unstable_revalidate("/idea/" + req.query.id);
   return res.status(200).json({ message: "revalidate success" });
 }
