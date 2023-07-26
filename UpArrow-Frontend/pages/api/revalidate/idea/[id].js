cc60b3c606a9ee03712752fc579d3c511a6633bb
pkiop <pkiopb@gmail.com>
2023-07-26 22:10:20 +0900

    guard id

--- UpArrow-Frontend/pages/api/revalidate/idea/[id].js
+++ UpArrow-Frontend/pages/api/revalidate/idea/[id].js
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
+  if (!req.params?.id) {
+    return res.status(400).json({ message: "missing id" });
+  }
   await res.unstable_revalidate("/idea/" + req.params.id);
   return res.status(200).json({ message: "revalidate success" });
 }
