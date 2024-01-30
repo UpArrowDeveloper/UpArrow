b06519ed584c1bb1b128c662d0428cb3d705ca29
pkiop <pkiopb@gmail.com>
2024-01-30 23:30:43 +0900

    favicon

--- UpArrow-Frontend/pages/_document.js
+++ UpArrow-Frontend/pages/_document.js
@@ -1,37 +1,39 @@
-import * as React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import createEmotionCache from '../src/createEmotionCache';
-import Script from 'next/script';
-import { Popup } from '../components/Popup';
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../src/createEmotionCache";
+import Script from "next/script";
+import { Popup } from "../components/Popup";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <link rel='shortcut icon' href='/static/favicon.ico' />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <meta name='emotion-insertion-point' content='' />
+          <meta name="emotion-insertion-point" content="" />
           <Script
-            src='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js'
-            integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl'
-            crossOrigin='true'
+            src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossOrigin="true"
           ></Script>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='true'
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Merriweather&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300&family=Sacramento&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Merriweather&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300&family=Sacramento&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+            rel="stylesheet"
           ></link>
+          disabled={!userUrl}
+          <link rel="apple-touch-icon" href="/static/favicon.ico"></link>
           {this.props.emotionStyleTags}
         </Head>
         <body>
@@ -89,7 +91,7 @@ MyDocument.getInitialProps = async (ctx) => {
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
       key={style.key}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
