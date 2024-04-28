4a1ffbdd8552671790b3ce06fb5333e2d84e3309
pkiop <pkiopb@gmail.com>
2024-04-28 22:06:15 +0900

    fix meta

--- UpArrow-Frontend/pages/editor/index.js
+++ UpArrow-Frontend/pages/editor/index.js
@@ -1,5 +1,6 @@
 import Editor from "../../components/Editor";
 import { MainLayout } from "../../Layouts";
+import Head from "next/head";
 
 const App = () => {
   return <Editor />;
@@ -7,9 +8,17 @@ const App = () => {
 
 function EditorPage(props) {
   return (
-    <MainLayout>
-      <App {...props} />
-    </MainLayout>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        ></meta>
+      </Head>
+      <MainLayout>
+        <App {...props} />
+      </MainLayout>
+    </>
   );
 }
 
